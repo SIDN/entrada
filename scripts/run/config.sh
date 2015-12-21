e783f8256704b933a595f4760ed80b419adb3c00
maarten <maarteen.wullink@sidn.nl>
2015-12-21 14:29:47 +0100

    export vars

--- scripts/run/config.sh
+++ scripts/run/config.sh
@@ -9,37 +9,37 @@
 ############################################################
 
 #home dir of entrada
-ENTRADA_HOME=""
+export ENTRADA_HOME=""
 #tmp dir used for keep state files
-TMP_DIR="$ENTRADA_HOME/tmp/"
+export TMP_DIR="$ENTRADA_HOME/tmp/"
 
 #decoder config file
-CONFIG_FILE="../config/entrada-settings.properties"
+export CONFIG_FILE="../config/entrada-settings.properties"
 
 #Impala deamon hostname for impala-shell to connect to
-IMPALA_NODE=""
+export IMPALA_NODE=""
 
 #hdfs locations for storing data
-HDFS_HOME=""
+export HDFS_HOME=""
 
 #input directories, subdirs must have same name as name server
-DATA_RSYNC_DIR="/home/captures"
+export DATA_RSYNC_DIR="/home/captures"
 #root directory for data input/output
-DATA_DIR=""
+export DATA_DIR=""
 
 #Log location
-ENTRADA_LOG_DIR="/var/log/entrada"
+export ENTRADA_LOG_DIR="/var/log/entrada"
 
 #name servers, seperate multiple NS with a colon ;
-NAMESERVERS=""
+export NAMESERVERS=""
 
 #java
-JAVA_BIN="/usr/lib/jvm/java-7-oracle/bin/java"
-ENTRADA_JAR="pcap-to-parquet-0.0.1-jar-with-dependencies.jar"
+export JAVA_BIN="/usr/lib/jvm/java-7-oracle/bin/java"
+export ENTRADA_JAR="pcap-to-parquet-0.0.1-jar-with-dependencies.jar"
 
 #security if Kerberos is enabled, otherwise keep empty
-KRB_USER=user@REALM
-KEYTAB_FILE=""
+export KRB_USER=user@REALM
+export KEYTAB_FILE=""
 
 #error mail recipient
-ERROR_MAIL=""
+export ERROR_MAIL=""

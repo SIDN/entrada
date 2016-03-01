9bcaa426ab1af773fbb8eb32fcae0992c421f524
Matteo Pergolesi <thewall@localhost.localdomain>
2016-03-01 10:47:56 +0100

    Deleting not needed '.gz' file extension (gzip command will add that for you).

--- scripts/capture/capture.sh
+++ scripts/capture/capture.sh
@@ -29,4 +29,4 @@ HOST_V6=
 HOSTNAME_PREFIX=
 
 #use  & 0x1fff != 0 to also capture fragmented packets
-tcpdump -i eth1 -w ${DUMPDIR}/$HOSTNAME_PREFIX_%Y-%m-%d_%H:%M.pcap.gz -G 300 -z gzip '(( port 53 or ip[6:2] & 0x1fff != 0) or icmp or icmp6 ) and ( host $HOST_V4 or host $HOST_V6 )' 
+tcpdump -i eth1 -w ${DUMPDIR}/$HOSTNAME_PREFIX_%Y-%m-%d_%H:%M.pcap -G 300 -z gzip '(( port 53 or ip[6:2] & 0x1fff != 0) or icmp or icmp6 ) and ( host $HOST_V4 or host $HOST_V6 )' 

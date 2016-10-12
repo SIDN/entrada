9ae3a43cf9183b6022ad152df3c7961bf811fa5c
maarten <maarteen.wullink@sidn.nl>
2016-10-12 13:47:23 +0200

    added support for multiple partitions, see #23

--- scripts/run/run_03_staging_to_warehouse.sh
+++ scripts/run/run_03_staging_to_warehouse.sh
@@ -47,68 +47,63 @@ runasSuperuser(){
 
 runasImpala
 
-if [ -z "$1" ]
-then
-  #get date for yesterday
-  day=$(date -d "1 days ago" +"%-d")
-  year=$(date -d "1 days ago" +"%Y")
-  month=$(date -d "1 days ago" +"%-m")
-else
-  #get date for parameter date, fomat should be YYYY-MM-DD
-  day=$(date -d "$1" +"%-d")
-  year=$(date -d "$1" +"%Y")
-  month=$(date -d "$1" +"%-m")  
-fi
-echo "[$(date)] : process staging partition: $day-$month-$year"
-
-
 ####
 #### DNS data section
 ####
 
-#insert all the staging data for yesterday into the datawarehouse table
-#skip the duplicate "svr" column.
-impala-shell -i $IMPALA_NODE -V -q  "insert into $IMPALA_DNS_DWH_TABLE partition(year, month, day, server) select 
-     id, unixtime, time, qname, domainname,
-     len, frag, ttl, ipv,
-     prot, src, srcp, dst,
-     dstp, udp_sum, dns_len, aa,
-     tc, rd, ra, z, ad, cd,
-     ancount, arcount, nscount, qdcount,
-     opcode, rcode, qtype, qclass,
-     country, asn, edns_udp, edns_version,
-     edns_do, edns_ping, edns_nsid, edns_dnssec_dau,
-     edns_dnssec_dhu, edns_dnssec_n3u,
-     edns_client_subnet, edns_other,
-     edns_client_subnet_asn,
-     edns_client_subnet_country,
-     labels,res_len,time_micro,resp_frag,proc_time,is_google,is_opendns,
-     dns_res_len,server_location, year,month,day,server
-     from $IMPALA_DNS_STAGING_TABLE where year=$year and month=$month and day=$day;"
-
-if [ $? -ne 0 ]
-then
-    #send mail to indicate error
-    echo "[$(date)] : insert data into $IMPALA_DNS_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
-    exit 1
-fi
-
-echo "[$(date)] : issue refresh for $IMPALA_DNS_DWH_TABLE"
-impala-shell --quiet -i $IMPALA_NODE -V -q "refresh $IMPALA_DNS_DWH_TABLE;"
-if [ $? -ne 0 ]
-then
-    #send mail to indicate error
-    echo "[$(date)] : refresh metadata $IMPALA_DNS_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
-fi
-
-#delete partitions from staging
-for server in $(hdfs dfs -ls $HDFS_DNS_STAGING/year=$year/month=$month/day=$day/ | grep server= | cut -d= -f5)
+for partition in $(impala-shell -i $IMPALA_NODE -q "select year,month,day,server from $IMPALA_DNS_STAGING_TABLE group by year,month,day,server order by year,month,day,server desc;" --output_delimiter=, --quiet -k --delimited)
 do
-    echo "[$(date)] : drop table $IMPALA_DNS_STAGING_TABLE partition for $server"
+    year=$(echo $partition | cut -d, -f 1)
+    month=$(echo $partition | cut -d, -f 2)
+    day=$(echo $partition | cut -d, -f 3)
+    server=$(echo $partition | cut -d, -f 4)
+
+    echo "[$(date)] : Move $IMPALA_DNS_STAGING_TABLE table partition year=$year, month=$month, day=$day, server=$server to $IMPALA_DNS_DWH_TABLE"
+
+    #insert all the staging data for yesterday into the datawarehouse table
+    #skip the duplicate "svr" column.
+    impala-shell -i $IMPALA_NODE -V -q  "insert into $IMPALA_DNS_DWH_TABLE partition(year, month, day, server) select 
+         id, unixtime, time, qname, domainname,
+         len, frag, ttl, ipv,
+         prot, src, srcp, dst,
+         dstp, udp_sum, dns_len, aa,
+         tc, rd, ra, z, ad, cd,
+         ancount, arcount, nscount, qdcount,
+         opcode, rcode, qtype, qclass,
+         country, asn, edns_udp, edns_version,
+         edns_do, edns_ping, edns_nsid, edns_dnssec_dau,
+         edns_dnssec_dhu, edns_dnssec_n3u,
+         edns_client_subnet, edns_other,
+         edns_client_subnet_asn,
+         edns_client_subnet_country,
+         labels,res_len,time_micro,resp_frag,proc_time,is_google,is_opendns,
+         dns_res_len,server_location, year,month,day,server
+         from $IMPALA_DNS_STAGING_TABLE where year=$year and month=$month and day=$day and server=$server;"
+
+    if [ $? -ne 0 ]
+    then
+        #send mail to indicate error
+        echo "[$(date)] : insert data into $IMPALA_DNS_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
+        exit 1
+    fi
+
+    #get new rows count
+    rows=$(impala-shell --quiet -i $IMPALA_NODE -q "select count(1) from $IMPALA_DNS_DWH_TABLE where year=$year and month=$month and day=$day and server=$server;" --quiet -B)
+    echo "[$(date)] : added $rows new rows to $IMPALA_DNS_DWH_TABLE"
+
+    #drop partition from the staging table (unlink parquet files)
+    echo "[$(date)] : drop $IMPALA_DNS_STAGING_TABLE partition (year=$year,month=$month,day=$day,server=$server)"
     impala-shell -c --quiet -i $IMPALA_NODE -V -q "alter table $IMPALA_DNS_STAGING_TABLE drop partition (year=$year,month=$month,day=$day, server=\"$server\");"
+
+    #delete staging parquet data from hdfs
+    runasSuperuser
+    echo "[$(date)] : delete the staging parquet files from hdfs $HDFS_DNS_STAGING/year=$year/month=$month/day=$day/server=$server"
+    hdfs dfs -rm -r -f $HDFS_DNS_STAGING/year=$year/month=$month/day=$day/server=$server  
 done
 
-#refresh impala metadata
+runasImpala
+
+#refresh impala metadata for staging table
 echo "[$(date)] : issue refresh for $IMPALA_DNS_STAGING_TABLE"
 impala-shell --quiet -i $IMPALA_NODE -V -q "refresh $IMPALA_DNS_STAGING_TABLE;"
 if [ $? -ne 0 ]
@@ -117,16 +112,6 @@ then
      echo "[$(date)] : refresh metadata $IMPALA_DNS_STAGING_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
 fi
 
-#get new rows count
-rows=$(impala-shell --quiet -i $IMPALA_NODE -q "select count(1) from $IMPALA_DNS_DWH_TABLE where year=$year and month=$month and day=$day;" --quiet -B)
-echo "[$(date)] : added $rows new rows to $IMPALA_DNS_DWH_TABLE"
-   
-#delete staging data from hdfs
-runasSuperuser
-echo "[$(date)] : delete the staging parquet files from hdfs $HDFS_DNS_STAGING/year=$year/month=$month/day=$day"
-hdfs dfs -rm -r -f $HDFS_DNS_STAGING/year=$year/month=$month/day=$day
-runasImpala
-
 #update the table statistics
 impala-shell -c --quiet -i $IMPALA_NODE -q "COMPUTE INCREMENTAL STATS $IMPALA_DNS_DWH_TABLE;" 
  
@@ -134,53 +119,65 @@ impala-shell -c --quiet -i $IMPALA_NODE -q "COMPUTE INCREMENTAL STATS $IMPALA_DN
 #### ICMP data section
 ####
 
-#skip the duplicate "svr" column.
-impala-shell -i $IMPALA_NODE -V -q  "insert into $IMPALA_ICMP_DWH_TABLE partition(year, month, day) select 
-      unixtime,icmp_type,
-      icmp_code,icmp_echo_client_type,ip_ttl,
-      ip_v,ip_src,
-      ip_dst,ip_country,
-      ip_asn,ip_len,
-      l4_prot,l4_srcp,
-      l4_dstp,orig_ip_ttl,
-      orig_ip_v,orig_ip_src,
-      orig_ip_dst,orig_l4_prot,
-      orig_l4_srcp,orig_l4_dstp,
-      orig_udp_sum,orig_ip_len,
-      orig_icmp_type,orig_icmp_code,
-      orig_icmp_echo_client_type,
-      orig_dns_id,orig_dns_qname,
-      orig_dns_domainname,orig_dns_len,
-      orig_dns_aa,orig_dns_tc,
-      orig_dns_rd,orig_dns_ra,
-      orig_dns_z,orig_dns_ad,
-      orig_dns_cd,orig_dns_ancount,
-      orig_dns_arcount,orig_dns_nscount,
-      orig_dns_qdcount,orig_dns_rcode,
-      orig_dns_qtype,orig_dns_opcode,
-      orig_dns_qclass,orig_dns_edns_udp,
-      orig_dns_edns_version,orig_dns_edns_do,
-      orig_dns_labels,svr,time_micro, server_location, year,month,day
-     from $IMPALA_ICMP_STAGING_TABLE where year=$year and month=$month and day=$day;"
+for partition in $(impala-shell -i $IMPALA_NODE -q "select year,month,day from $IMPALA_ICMP_STAGING_TABLE group by year,month,day order by year,month,day desc;" --output_delimiter=, --quiet -k --delimited)
+do
+    year=$(echo $partition | cut -d, -f 1)
+    month=$(echo $partition | cut -d, -f 2)
+    day=$(echo $partition | cut -d, -f 3)
 
-if [ $? -ne 0 ]
-then
-    #send mail to indicate error
-    echo "[$(date)] : insert data into $IMPALA_ICMP_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
-    exit 1
-fi
+    echo "[$(date)] : Move $IMPALA_ICMP_STAGING_TABLE table partition year=$year, month=$month, day=$day to $IMPALA_ICMP_DWH_TABLE"
 
-echo "[$(date)] : issue refresh for $IMPALA_ICMP_DWH_TABLE"
-impala-shell --quiet -i $IMPALA_NODE -V -q "refresh $IMPALA_ICMP_DWH_TABLE;"
-if [ $? -ne 0 ]
-then
-     #send mail to indicate error
-     echo "[$(date)] : refresh metadata $IMPALA_ICMP_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
-fi
+    #skip the duplicate "svr" column.
+    impala-shell -i $IMPALA_NODE -V -q  "insert into $IMPALA_ICMP_DWH_TABLE partition(year, month, day) select 
+          unixtime,icmp_type,
+          icmp_code,icmp_echo_client_type,ip_ttl,
+          ip_v,ip_src,
+          ip_dst,ip_country,
+          ip_asn,ip_len,
+          l4_prot,l4_srcp,
+          l4_dstp,orig_ip_ttl,
+          orig_ip_v,orig_ip_src,
+          orig_ip_dst,orig_l4_prot,
+          orig_l4_srcp,orig_l4_dstp,
+          orig_udp_sum,orig_ip_len,
+          orig_icmp_type,orig_icmp_code,
+          orig_icmp_echo_client_type,
+          orig_dns_id,orig_dns_qname,
+          orig_dns_domainname,orig_dns_len,
+          orig_dns_aa,orig_dns_tc,
+          orig_dns_rd,orig_dns_ra,
+          orig_dns_z,orig_dns_ad,
+          orig_dns_cd,orig_dns_ancount,
+          orig_dns_arcount,orig_dns_nscount,
+          orig_dns_qdcount,orig_dns_rcode,
+          orig_dns_qtype,orig_dns_opcode,
+          orig_dns_qclass,orig_dns_edns_udp,
+          orig_dns_edns_version,orig_dns_edns_do,
+          orig_dns_labels,svr,time_micro, server_location, year,month,day
+         from $IMPALA_ICMP_STAGING_TABLE where year=$year and month=$month and day=$day;"
 
+    if [ $? -ne 0 ]
+    then
+        #send mail to indicate error
+        echo "[$(date)] : insert data into $IMPALA_ICMP_DWH_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
+        exit 1
+    fi
 
-#delete partition from staging
-impala-shell -c --quiet -i $IMPALA_NODE -V -q "alter table $IMPALA_ICMP_STAGING_TABLE drop partition (year=$year,month=$month,day=$day);"
+    #get new rows count
+    rows=$(impala-shell --quiet -i $IMPALA_NODE -q "select count(1) from $IMPALA_ICMP_DWH_TABLE where year=$year and month=$month and day=$day;" --quiet -B)
+    echo "[$(date)] : added $rows new rows to $IMPALA_ICMP_DWH_TABLE"
+
+    #delete partition from staging
+    echo "[$(date)] : drop $IMPALA_ICMP_STAGING_TABLE partition (year=$year,month=$month,day=$day)"
+    impala-shell -c --quiet -i $IMPALA_NODE -V -q "alter table $IMPALA_ICMP_STAGING_TABLE drop partition (year=$year,month=$month,day=$day);"
+
+    #delete staging data from hdfs
+    runasSuperuser
+    echo "[$(date)] : delete the staging parquet files from hdfs $HDFS_ICMP_STAGING/year=$year/month=$month/day=$day"
+    hdfs dfs -rm -r -f $HDFS_ICMP_STAGING/year=$year/month=$month/day=$day
+done
+
+runasImpala
 
 #refresh impala metadata
 echo "[$(date)] : issue refresh for $IMPALA_ICMP_STAGING_TABLE"
@@ -190,16 +187,6 @@ then
      #send mail to indicate error
      echo "[$(date)] : refresh metadata $IMPALA_ICMP_STAGING_TABLE failed" | mail -s "Impala error" $ERROR_MAIL
 fi
-
-#get new rows count
-rows=$(impala-shell --quiet -i $IMPALA_NODE -q "select count(1) from $IMPALA_ICMP_DWH_TABLE where year=$year and month=$month and day=$day;" --quiet -B)
-echo "[$(date)] : added $rows new rows to $IMPALA_ICMP_DWH_TABLE"
-   
-#delete staging data from hdfs
-runasSuperuser
-echo "[$(date)] : delete the staging parquet files from hdfs $HDFS_ICMP_STAGING/year=$year/month=$month/day=$day"
-hdfs dfs -rm -r -f $HDFS_ICMP_STAGING/year=$year/month=$month/day=$day
-runasImpala
 
 #update the table statistics
 impala-shell --quiet -i $IMPALA_NODE -q "COMPUTE INCREMENTAL STATS $IMPALA_ICMP_DWH_TABLE;" 

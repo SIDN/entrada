77341bf2a632e083c6f8328b87e588c2a247ac61
Oli Schacher <oli.schacher@switch.ch>
2017-08-10 18:40:04 +0200

    add support for erroneously present fields in the query, #47

--- scripts/database/upgrade/to_0.0.9.sql
+++ scripts/database/upgrade/to_0.0.9.sql
@@ -1,10 +1,10 @@
 use dns;
 
 alter table staging
-add columns (edns_padding INT, pcap_file STRING, edns_keytag_count INT, edns_keytag_list STRING);
+add columns (edns_padding INT, pcap_file STRING, edns_keytag_count INT, edns_keytag_list STRING, q_tc BOOLEAN, q_ra BOOLEAN, q_ad BOOLEAN, q_rcode INT );
 
 alter table queries
-add columns (edns_padding INT, pcap_file STRING, edns_keytag_count INT, edns_keytag_list STRING);
+add columns (edns_padding INT, pcap_file STRING, edns_keytag_count INT, edns_keytag_list STRING, q_tc BOOLEAN, q_ra BOOLEAN, q_ad BOOLEAN, q_rcode INT);
 
 use icmp;
 

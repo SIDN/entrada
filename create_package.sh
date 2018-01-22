7befb722a15af59ea744227918045624e632523f
maarten <maarten.wullink@sidn.nl>
2018-01-22 09:01:11 +0100

    fix

--- create_package.sh
+++ create_package.sh
@@ -4,6 +4,9 @@ VERSION=$(cat VERSION)
 BASE_DIR="entrada-$VERSION"
 echo "Create ENTRADA installation package for version $VERSION"
 
+#make 100% sure the package dir does not exist
+rm -rf $BASE_DIR
+
 mkdir $BASE_DIR
 cp pcap-to-parquet/target/pcap-to-parquet-$VERSION-jar-with-dependencies.jar $BASE_DIR
 cp -R scripts $BASE_DIR

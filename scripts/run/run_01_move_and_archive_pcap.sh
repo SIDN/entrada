eb824315b8b839a62e0ffc7044c44eacdb662834
Markus Videfors <markus.videfors@iis.se>
2019-02-14 10:15:03 +0100

    Added functions necessary for avoiding automation errors

--- scripts/run/run_01_move_and_archive_pcap.sh
+++ scripts/run/run_01_move_and_archive_pcap.sh
@@ -1,1 +1,40 @@
 #!/usr/bin/env bash
+
+# This bash script is made to move incoming pcap files from S3 to local storage
+# and then copy those files onto an archive built in S3.
+
+INPUT_FILTER="*.pcap"
+SOURCE="$S3_HOME/input"
+OUTPUT_DIR="$DATA_DIR/processing"
+PID=$TMP_DIR/moving_pcaps
+
+# copied with modifications from the original scripts:
+cleanup(){
+  #remove pid file
+  if [ -f $PID ];
+  then
+    rm $PID
+  fi
+}
+
+echo "[$(date)] : starting movement of pcaps"
+
+if [ -f $PID ];
+then
+  echo "[$(date)] : $PID  : Process is already running, do not start new process."
+  exit 1
+fi
+
+#check if tmp dir exists, if not create it
+if ! [ -f "$TMP_DIR" ]
+then
+  mkdir -p $TMP_DIR
+fi
+
+#create pid file
+echo 1 > $PID
+
+#Make sure cleanup() is called when script is done processing or crashed.
+trap cleanup EXIT
+
+#

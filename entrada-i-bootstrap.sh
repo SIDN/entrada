5cade805f24aeed9e2ec359527ff814eee7dd13f
Markus Videfors <markus.videfors@iis.se>
2019-02-20 10:44:40 +0100

    Gather table statistics

--- entrada-i-bootstrap.sh
+++ entrada-i-bootstrap.sh
@@ -36,6 +36,15 @@ sudo chmod -R 700 ./
 sh ./entrada-latest/scripts/install/create_s3External_tables.sh && sh ./entrada-latest/scripts/install/create_domain_stats_table_S3.sh
 # detect already existing partitions (and thereby data) on these tables
 hive -e "MSCK REPAIR TABLE $DNS_STAGING_TABLE; MSCK REPAIR TABLE $DNS_DWH_TABLE; MSCK REPAIR TABLE dns.domain_query_stats;"
+# gather table statistics
+hive -e "
+ANALYZE TABLE $DNS_STAGING_TABLE PARTITION(year, month, day, server) COMPUTE STATISTICS;
+ANALYZE TABLE $DNS_STAGING_TABLE PARTITION(year, month, day, server) COMPUTE STATISTICS for columns;
+ANALYZE TABLE $DNS_DWH_TABLE PARTITION(year, month, day, server) COMPUTE STATISTICS;
+ANALYZE TABLE $DNS_DWH_TABLE PARTITION(year, month, day, server) COMPUTE STATISTICS for columns;
+ANALYZE TABLE dns.domain_query_stats PARTITION(year, month, day) COMPUTE STATISTICS;
+ANALYZE TABLE dns.domain_query_stats PARTITION(year, month, day) COMPUTE STATISTICS for columns;
+"
 
 #create log dir and set up logrotate
 sudo mkdir -p /var/log/entrada

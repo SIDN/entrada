488e9e9a9e37c2d9d52b7064fef8c50079a016aa
Markus Videfors <markus.videfors@iis.se>
2019-03-06 14:12:41 +0100

    int instead of str pls

--- countInput.py
+++ countInput.py
@@ -2,7 +2,7 @@
 import sys, boto3
 
 def main(bucket, prefix, max):
-    print(boto3.client("s3").list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=max)["KeyCount"])
+    print(boto3.client("s3").list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=int(max))["KeyCount"])
 
 if __name__ == "__main__":
     main(*sys.argv[1:])

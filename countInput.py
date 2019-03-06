8ce68c0ccbc86a53e90fd17eb92dc22bde2be979
Markus Videfors <markus.videfors@iis.se>
2019-03-06 09:55:27 +0100

    need to count input files for processing

---
+++ countInput.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys, boto3
+
+def main(bucket, prefix, max):
+    print(boto3.client("s3").list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=max)["KeyCount"])
+
+if __name__ == "__main__":
+    main()

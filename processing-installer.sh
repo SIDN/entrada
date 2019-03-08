b31b1c9ed43fc012145cafab6e96cce06ca60554
Markus Videfors <markus.videfors@iis.se>
2019-03-08 11:01:19 +0100

    create processing subdir

--- processing-installer.sh
+++ processing-installer.sh
@@ -8,7 +8,7 @@ source entrada-latest/scripts/run/config.sh 2>&1 /dev/null
 
 #create directories for processing
 echo "[$(date)] : Making pcap directories"
-sudo mkdir -p $DATA_DIR
+sudo mkdir -p $DATA_DIR/processing
 sudo mkdir -p $OUTPUT_DIR
 
 # Find an unpartitioned disk and mount it on $DATA_DIR. Will use whichever unpartitioned

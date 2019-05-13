2e6286e884e989d7297387c7816f8526bbfc8093
Markus Videfors <markus.videfors@iis.se>
2019-05-13 11:19:39 +0200

    Cleanup of files no longer used

--- scripts/run/countInput.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys, boto3
-
-def main(bucket, prefix, max):
-    print(boto3.client("s3").list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=int(max))["KeyCount"])
-
-if __name__ == "__main__":
-    main(*sys.argv[1:])

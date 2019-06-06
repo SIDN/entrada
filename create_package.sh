7602bc5c21e2cbc867ed20bebf029690353d0673
Maarten Wullink <maartenwullink@dhcp140.sidnlabs.nl>
2019-06-06 13:25:47 +0200

    fix script

--- create_package.sh
+++ create_package.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-VERSION=$(cat VERSION)
+VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | egrep -v '^\[|Downloading:' | tr -d ' \n')
 BASE_DIR="entrada-$VERSION"
 echo "Create ENTRADA installation package for version $VERSION"
 
-echo "Workspace: $WORKSPACE"
-
 #make 100% sure the package dir does not exist
 rm -rf $BASE_DIR
-
 mkdir $BASE_DIR
-cp $WORKSPACE/target/entrada-$VERSION-jar-with-dependencies.jar $BASE_DIR
-cp -R scripts $BASE_DIR
-cp -R grafana-dashboard $BASE_DIR
-cp VERSION $BASE_DIR
-cp UPGRADE $BASE_DIR
+
+cp ./target/entrada-$VERSION-jar-with-dependencies.jar $BASE_DIR
+cp -R ./scripts $BASE_DIR
+cp -R ./grafana-dashboard $BASE_DIR
+cp ./UPGRADE $BASE_DIR
 cd $BASE_DIR
 ln -s entrada-$VERSION-jar-with-dependencies.jar entrada-latest.jar
 cd ..
